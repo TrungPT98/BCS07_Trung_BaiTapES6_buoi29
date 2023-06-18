97c8e36e733b0bfae06b8d9441e989fc994ba95a
ThanhTrung_Dev <trungpt3898@gmail.com>
2023-06-18 15:43:18 +0700

    Sửa chức năng thêm lần 2

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+// console.log("Hello");
 // import Person, { HocVien } from "../models/Person.js";
 import { HocVien } from "../models/Person.js";
 import { NhanVien } from "../models/Person.js";
@@ -7,63 +7,160 @@ import ListPerson from "../models/ListPerson.js";
 
 let listPerson = new ListPerson();
 
+document.getElementById('firstDisabled').addEventListener('click', () => {
+    document.getElementById('soNgayLam').disabled = true;
+    document.getElementById('luongNgay').disabled = true;
+    document.getElementById('tenCty').disabled = true;
+    document.getElementById('triGiaHoaDon').disabled = true;
+    document.getElementById('danhGia').disabled = true;
+})
+
 document.getElementById('btnThemHocVien').addEventListener
-('click', () => {
-    let arrInput = document.querySelectorAll('#personFormHocVien input');
+    ('click', () => {
 
-    let hocVien = new HocVien();
+        let loaiPerson = document.getElementById('loaiPerson').value;
+        let arrInput = document.querySelectorAll('#personForm input, #personForm select');
 
-    for (let item of arrInput) {
-        let { id, value } = item;
-        hocVien[id] = value;
-    }
-    console.log(hocVien);
-    listPerson.themHocVien(hocVien);
-    listPerson.renderHocVien();
-});
+        if (loaiPerson == "hocVien") {
+            // let arrInput = document.querySelectorAll('#personForm input');
 
-document.getElementById('btnThemNhanVien').addEventListener
-('click', () => {
-    let arrInput = document.querySelectorAll('#personFormNhanVien input');
+            let hocVien = new HocVien();
 
-    let nhanVien = new NhanVien();
+            for (let item of arrInput) {
+                let { id, value } = item;
+                hocVien[id] = value;
+            }
+            console.log(hocVien);
+            listPerson.themPerson(hocVien);
+            // listPerson.renderPerson();
+        } else if (loaiPerson == "nhanVien") {
+            // let arrInput = document.querySelectorAll('#personForm input');
 
-    for (let item of arrInput) {
-        let { id, value } = item;
-        nhanVien[id] = value;
-    }
-    console.log(nhanVien);
-    listPerson.themNhanVien(nhanVien);
-    listPerson.renderNhanVien();
-});
+            let nhanVien = new NhanVien();
 
-document.getElementById('btnThemKhachHang').addEventListener
-('click', () => {
-    let arrInput = document.querySelectorAll('#personFormKhachHang input, #personFormKhachHang select');
+            for (let item of arrInput) {
+                let { id, value } = item;
+                nhanVien[id] = value;
+            }
+            console.log(nhanVien);
+            listPerson.themPerson(nhanVien);
+            // listPerson.renderPerson();
+        } else if (loaiPerson == "khachHang") {
+            // let arrInput = document.querySelectorAll('#personForm input, #personForm select');
 
-    let khachHang = new KhachHang();
+            let khachHang = new KhachHang();
 
-    for (let item of arrInput) {
-        let { id, value } = item;
-        khachHang[id] = value;
-    }
-    console.log(khachHang);
-    listPerson.themKhachHang(khachHang);
-    listPerson.renderKhachHang();
-});
+            for (let item of arrInput) {
+                let { id, value } = item;
+                khachHang[id] = value;
+            }
+            console.log(khachHang);
+            listPerson.themPerson(khachHang);
+        }
+        console.log(arrInput);
+        listPerson.renderPerson();
+
+
+    });
+
+// document.getElementById('btnThemNhanVien').addEventListener
+//     ('click', () => {
+//         let arrInput = document.querySelectorAll('#personFormNhanVien input');
+
+//         let nhanVien = new NhanVien();
+
+//         for (let item of arrInput) {
+//             let { id, value } = item;
+//             nhanVien[id] = value;
+//         }
+//         console.log(nhanVien);
+//         listPerson.themNhanVien(nhanVien);
+//         listPerson.renderNhanVien();
+//     });
+
+// document.getElementById('btnThemKhachHang').addEventListener
+//     ('click', () => {
+//         let arrInput = document.querySelectorAll('#personFormKhachHang input, #personFormKhachHang select');
+
+//         let khachHang = new KhachHang();
+
+//         for (let item of arrInput) {
+//             let { id, value } = item;
+//             khachHang[id] = value;
+//         }
+//         console.log(khachHang);
+//         listPerson.themKhachHang(khachHang);
+//         listPerson.renderKhachHang();
+//     });
 
 // VD về Destructuring
-let pet = {
-    name: 'Gâu',
-    age: 3,
-    size: {
-        wei: '30kg',
-        hei: '56cm'
-    }
-}
+// let pet = {
+//     name: 'Gâu',
+//     age: 3,
+//     size: {
+//         wei: '30kg',
+//         hei: '56cm'
+//     }
+// }
 
-let {name, age} = pet;
-let {wei, hei} = pet.size;
-console.log(name, age);
-console.log(wei, hei);
+// let { name, age } = pet;
+// let { wei, hei } = pet.size;
+// console.log(name, age);
+// console.log(wei, hei);
+
+document.getElementById('loaiPerson').addEventListener('change', () => {
+    let loaiPerson = document.getElementById('loaiPerson').value;
+    if (loaiPerson == "hocVien") {
+        document.getElementById('diemToan').disabled = false;
+        document.getElementById('diemLy').disabled = false;
+        document.getElementById('diemHoa').disabled = false;
+
+        document.getElementById('soNgayLam').disabled = true;
+        document.getElementById('luongNgay').disabled = true;
+        document.getElementById('tenCty').disabled = true;
+        document.getElementById('triGiaHoaDon').disabled = true;
+        document.getElementById('danhGia').disabled = true;
+    } else if (loaiPerson == "nhanVien") {
+        document.getElementById('soNgayLam').disabled = false;
+        document.getElementById('luongNgay').disabled = false;
+
+        document.getElementById('diemToan').disabled = true;
+        document.getElementById('diemLy').disabled = true;
+        document.getElementById('diemHoa').disabled = true;
+        document.getElementById('tenCty').disabled = true;
+        document.getElementById('triGiaHoaDon').disabled = true;
+        document.getElementById('danhGia').disabled = true;
+    } else if (loaiPerson == "khachHang") {
+        document.getElementById('tenCty').disabled = false;
+        document.getElementById('triGiaHoaDon').disabled = false;
+        document.getElementById('danhGia').disabled = false;
+
+        document.getElementById('diemToan').disabled = true;
+        document.getElementById('diemLy').disabled = true;
+        document.getElementById('diemHoa').disabled = true;
+        document.getElementById('soNgayLam').disabled = true;
+        document.getElementById('luongNgay').disabled = true;
+    }
+})
+// function chonLoaiPerson() {
+//     var loaiPerson = document.getElementById('loaiPerson').value * 1;
+//     if (loaiPerson == "hocVien") {
+//         document.getElementById('diemToan').disabled = false;
+//         document.getElementById('diemLy').disabled = false;
+//         document.getElementById('diemHoa').disabled = false;
+
+//         document.getElementById('soNgayLam').disabled = true;
+//         document.getElementById('luongNgay').disabled = true;
+//         document.getElementById('tenCty').disabled = true;
+//         document.getElementById('triGiaHoaDon').disabled = true;
+//         document.getElementById('danhGia').disabled = true;
+//     } else if (loaiPerson == "nhanVien") {
+//         document.getElementById('soNgayLam').disabled = false;
+//         document.getElementById('luongNgay').disabled = false;
+//     } else if (loaiPerson == "khachHang") {
+//         document.getElementById('tenCty').disabled = false;
+//         document.getElementById('triGiaHoaDon').disabled = false;
+//         document.getElementById('danhGia').disabled = false;
+//     }
+// }
 
--- app/models/ListPerson.js
+++ app/models/ListPerson.js
@@ -6,41 +6,131 @@ import { KhachHang } from "./Person.js";
 export default class ListPerson {
     constructor() {
         this.arrListPerson = [];
-        this.arrListHocVien = [];
-        this.arrListNhanVien = [];
-        this.arrListKhachHang = [];
+        // this.arrListHocVien = [];
+        // this.arrListNhanVien = [];
+        // this.arrListKhachHang = [];
     }
-    themHocVien(person) {
-        this.arrListHocVien.push(person);
+    themPerson(person) {
+        this.arrListPerson.push(person);
     }
 
-    renderHocVien() {
+    renderPerson() {
         // let contentPerson = 
         // let person = new Person();
         // console.log(person);
-        let content = this.arrListHocVien.map((item, index) => {
-            let hocVien = new HocVien();
-            Object.assign(hocVien, item);
+        let loaiPerson = document.getElementById('loaiPerson').value;
+        let content = "";
 
-            let { personID, hoTen, email, diaChi, diemTB } = hocVien;
-            console.log(hocVien);
-            return `
-            <tr>
-                <td>${personID}</td>
-                <td>${hoTen}</td>
-                <td>${email}</td>
-                <td>${diaChi}</td>
-                <td>${diemTB()}</td>
-                <td></td>
-                <td>
-                <button class="btn btn-danger" onclick="xoaPerson('${personID}')">Xoá</button>
-                <button class="btn btn-warning" onclick="layThongTinPerson('${personID}')">Sửa</button>
-                </td>
-            </tr>
-            `
-        });
-        console.log(content);
+        if (loaiPerson == "hocVien") {
+            this.arrListPerson.map((item, index) => {
+                let hocVien = new HocVien();
+                Object.assign(hocVien, item);
+
+                let { personID, hoTen, email, diaChi, diemTB } = hocVien;
+                console.log(hocVien);
+                return content += `
+                <tr>
+                    <td>${personID}</td>
+                    <td>${hoTen}</td>
+                    <td>${email}</td>
+                    <td>${diaChi}</td>
+                    <td>${diemTB()}</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                    <button class="btn btn-danger" onclick="xoaPerson('${personID}')">Xoá</button>
+                    <button class="btn btn-warning" onclick="layThongTinPerson('${personID}')">Sửa</button>
+                    </td>
+                </tr>
+                `
+            });
+            // console.log(content);
+            // document.getElementById('tbodyPerson').innerHTML = content;
+        } else if (loaiPerson == "nhanVien") {
+            this.arrListPerson.map((item, index) => {
+                let nhanVien = new NhanVien();
+                Object.assign(nhanVien, item);
+
+                let { personID, hoTen, email, diaChi, tinhLuong } = nhanVien;
+                console.log(nhanVien);
+                return content += `
+                <tr>
+                    <td>${personID}</td>
+                    <td>${hoTen}</td>
+                    <td>${email}</td>
+                    <td>${diaChi}</td>
+                    <td></td>
+                    <td>${tinhLuong()}</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                    <button class="btn btn-danger" onclick="xoaPerson('${personID}')">Xoá</button>
+                    <button class="btn btn-warning" onclick="layThongTinPerson('${personID}')">Sửa</button>
+                    </td>
+                </tr>
+                `
+            });
+            // console.log(content);
+            // document.getElementById('tbodyPerson').innerHTML = content;
+        } else if (loaiPerson == "khachHang") {
+            this.arrListPerson.map((item, index) => {
+                let khachHang = new KhachHang();
+                Object.assign(khachHang, item);
+
+                let { personID, hoTen, email, diaChi, tenCty, giaTriHoaDon, danhGia } = khachHang;
+                console.log(khachHang);
+                return content += `
+                <tr>
+                    <td>${personID}</td>
+                    <td>${hoTen}</td>
+                    <td>${email}</td>
+                    <td>${diaChi}</td>
+                    <td></td>
+                    <td></td>
+                    <td>${tenCty}</td>
+                    <td>${giaTriHoaDon}</td>
+                    <td>${danhGia}</td>
+                    <td>
+                    <button class="btn btn-danger" onclick="xoaPerson('${personID}')">Xoá</button>
+                    <button class="btn btn-warning" onclick="layThongTinPerson('${personID}')">Sửa</button>
+                    </td>
+                </tr>
+                `
+            });
+            // console.log(content);
+            // document.getElementById('tbodyPerson').innerHTML = content;
+        }
         document.getElementById('tbodyPerson').innerHTML = content;
+
+        // let content = this.arrListHocVien.map((item, index) => {
+        //     let hocVien = new HocVien();
+        //     Object.assign(hocVien, item);
+
+        //     let { personID, hoTen, email, diaChi, diemTB } = hocVien;
+        //     console.log(hocVien);
+        //     return `
+        //     <tr>
+        //         <td>${personID}</td>
+        //         <td>${hoTen}</td>
+        //         <td>${email}</td>
+        //         <td>${diaChi}</td>
+        //         <td>${diemTB()}</td>
+        //         <td></td>
+        //         <td></td>
+        //         <td></td>
+        //         <td></td>
+        //         <td>
+        //         <button class="btn btn-danger" onclick="xoaPerson('${personID}')">Xoá</button>
+        //         <button class="btn btn-warning" onclick="layThongTinPerson('${personID}')">Sửa</button>
+        //         </td>
+        //     </tr>
+        //     `
+        // });
+        // console.log(content);
+        // document.getElementById('tbodyPerson').innerHTML = content;
     }
 
     themNhanVien(person) {
